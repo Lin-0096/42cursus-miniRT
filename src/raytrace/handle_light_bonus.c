587d15819b2c8953f49792ab8da4ce15b1bf0ac9
Lin Liu <linliu@student.hive.fi>
2025-09-23 16:31:29 +0300

    fix some norm

--- src/raytrace/handle_light_bonus.c
+++ src/raytrace/handle_light_bonus.c
@@ -1,4 +1,4 @@
-#include "miniRT_bonus.h"  
+#include "miniRT_bonus.h"
 
 
 // if
@@ -22,30 +22,30 @@
 // }	t_hit_record;
 //yuxin added for checkerboard
 //after hit obj, use (x, z) to decide is it single or double
-// #include <stdlib.h> 
+// #include <stdlib.h>
 // int abs(int x); absolute value
 // floorf rounded a float down to an int
-// why floorf 
+// why floorf
 // intput x		(int)x				floorf(x)
-// -1.9			-1					-2 			
-// -1.2			-1					-2 			
-// -0.7			0					-1 			
+// -1.9			-1					-2
+// -1.2			-1					-2
+// -0.7			0					-1
 // 	0.7			0					0
 // 	1.2			1					1
 //				rounded towards 0	always round down
 static	t_color apply_checkerboard(t_hit_record rec, t_color original_color)
 {
-	int x;
-	int z; 
+	int	x;
+	int	z;
 
 	// x = rec.point.x;
-	// z = rec.point.z; 
+	// z = rec.point.z;
 	x = floorf(rec.point.x);
     z = floorf(rec.point.z);
 	if (((abs(x + z)) % 2) == 0) //double not change
 		return (original_color);
 	else //single reverse
-		return (t_color){255 - original_color.r, 255 - original_color.g, 255 - original_color.b}; 
+		return (t_color){255 - original_color.r, 255 - original_color.g, 255 - original_color.b};
 }
 
 //this one from lin, just need to apply checkerboard color here
@@ -63,9 +63,9 @@ t_color	final_color(t_scene *scene, t_hit_record rec)
 	t_color	obj_color;
 
 	obj_color = get_color_from_object(rec.obj);
-	obj_color = apply_checkerboard(rec, obj_color);  //this line added
+	obj_color = apply_checkerboard(rec, obj_color);
 	ambient = apply_ambient(obj_color, scene->ambient_light);
-	if(is_in_shadow(rec, scene->light, scene->objects))
+	if (is_in_shadow(rec, scene->light, scene->objects))
 		diffuse = (t_color){0, 0, 0};
 	else
 		diffuse = apply_diffuse(obj_color, scene->light, rec);
